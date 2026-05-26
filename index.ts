d2364b575adff7c5059840e6ba48727ac2461edd
bogaledemasrepo <bogidemas@gmail.com>
2026-05-26 21:45:35 +0000

    dependency added drizzle configured running script added

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import express from "express";
+import { db } from "./db";
+import { users } from "./db/schema";
+
+const app = express();
+app.use(express.json());
+
+app.get("/users", async (req, res) => {
+  const allUsers = await db.select().from(users);
+  res.json(allUsers);
+});
+
+app.listen(3000, () => {
+  console.log(`Server running on ${process.env.HOST||"http://localhost:3000"}`);
+});
